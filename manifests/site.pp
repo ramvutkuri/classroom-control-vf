2421fe0348bb8c69ab248f2ed128317859f3855a
ramvutkuri <mohan.vutkuri.f11m@statefarm.com>
2016-05-23 15:57:02 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,6 +38,14 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+file { '/etc/motd':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+content => "Think before you type\n",
+}
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
