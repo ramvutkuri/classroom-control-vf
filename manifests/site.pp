97c50fe9823edce8688897112b3ebe38c96dc554
ramvutkuri <mohan.vutkuri.f11m@statefarm.com>
2016-05-24 09:44:38 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,10 +46,10 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
-  file { '/etc/motd/' :
+  file { '/etc/motd' :
     ensure => file,
     owner => 'root',
-    group => 'root'
+    group => 'root',
     mode => '0644',
     content => "I liked the puppet training",
     }
