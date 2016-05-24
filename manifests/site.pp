89aa3375a77d987cd3c39c4947a3ebb74a051f2b
ramvutkuri <mohan.vutkuri.f11m@statefarm.com>
2016-05-24 10:09:46 -0500

    updated the site.pp
    reintroduce "cowsay"

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,11 +46,15 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
-  file { '/etc/motd' :
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    content => "I liked the puppet training",
-    }
+  #file { '/etc/motd' :
+   # ensure => file,
+   # owner => 'root',
+   # group => 'root',
+    #mode => '0644',
+    #content => "I liked the puppet training",
+   # }
+   exec { "cowsay 'Welcome to ${::fqdn}' > /etc/motd" :
+   path => '/usr/bin:/usr/local/bin',
+   creates => '/etc/motd',
+   }
 }
