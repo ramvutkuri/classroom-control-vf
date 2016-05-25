32a98f5b24150e588237870de07e8d52f5ab9165
ramvutkuri <mohan.vutkuri.f11m@statefarm.com>
2016-05-25 09:03:57 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,7 +1,15 @@
 class nginx {
-package {'nginx' :
-ensure => present,
+  file {'nginx rpm' :
+ensure => file,
+path => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+source => 'puppet:///modules/nginx/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
 }
+package { 'nginx' :
+  ensure => '1.6.2-1.el7.centos.ngx',
+  source => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+  provider => rpm,
+  require => File['nginx rpm']
+  }
 
 file { '/var/www' :
 ensure => directory,
